f8e6aa90444a1bf4a1a9e692c35cfa7181a3c724
Masaho Tamura <tamuramasaho753@gmail.com>
2020-01-16 22:32:22 +0900

    add test

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ApplicationRecord
-    validates :name, presence: true
+   #  validates :name, presence: true
     validates :name, length: { maximum: 30 }
     validate :validate_name_not_including_comma
 
