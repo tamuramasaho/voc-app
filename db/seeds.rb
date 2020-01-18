5eb58b6738a387cb3d0a66459f69803e96e72f02
Masaho Tamura <tamuramasaho753@gmail.com>
2020-01-18 18:46:00 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,19 @@ User.create!(name: "Example User",
              password: "password",
              password_confirmation: "password",
              admin: true)
-             
+
 User.create!(name: "Example User-2",
              email: "example-2@example.com",
              password: "password",
              password_confirmation: "password",
              admin: false)
+
+users = User.take(2)
+50.times do
+    word = Faker::Verb.base
+    translation = Faker::Lorem.word
+    example = Faker::Address.full_address
+    users.each do |user| 
+        user.words.create!(name: word, translation: translation, example: example)
+    end
+end
