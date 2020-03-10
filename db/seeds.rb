8bdf7b20159a5dcd3a31dde212fa1cb572f3cf59
Masaho Tamura <tamuramasaho753@gmail.com>
2020-03-10 18:06:25 +0900

    add sample login information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-User.create!(name: "Example User",
-             email: "example@example.com",
+User.create!(name: "Tamura Masaho",
+             email: "tamuramasaho@gmail.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
 
-User.create!(name: "Example User-2",
-             email: "example-2@example.com",
+User.create!(name: "Kusano Minako",
+             email: "kusanominako@gmail.com",
              password: "password",
              password_confirmation: "password",
              admin: false)
